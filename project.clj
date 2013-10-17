125206e022953ceb07698b912810bb996521e219
Leo Franchi <lfranchi@kde.org>
2013-10-17 14:48:51 -0400

    core.async

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject cljs-intro "0.1.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [
+                 [org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1934"]
-                 [core.async "0.1.0-SNAPSHOT"]]
+                 [core.async "0.1.0-SNAPSHOT"]
+                 [crate "0.2.4"]
+                 [jayq "2.4.0"]]
 
   :source-paths ["src"]
 
@@ -13,10 +16,12 @@
       :source-paths ["src"]
       :compiler {:output-to "main.js"
                  :output-dir "out"
+                 :externs ["resources/js/externs/jquery-1.10.2.min.js"]
                  :optimizations :none
                  :pretty-print true}}
      {:id "node"
       :source-paths ["src"]
       :compiler {:output-to "main.js"
                  :optimizations :simple
+                 :externs ["resources/js/externs/jquery-1.10.2.min.js"]
                  :pretty-print true}}] })
--- src/intro/core.cljs
+++ src/intro/core.cljs
@@ -1,9 +1,33 @@
 (ns intro.core
   (:require [cljs.core.async :as async
-             :refer [<! >! chan close! sliding-buffer put! alts! timeout]])
+             :refer [<! >! chan close! sliding-buffer put! alts!]]
+            [clojure.browser.repl :as repl]
+            [crate.core :as crate]
+            [jayq.core :refer [$ append inner] :as jq]
+            )
   (:require-macros
     [cljs.core.async.macros :refer [go alt!]]))
 
-(js/console.log (+ 2 2))
+(repl/connect "http://localhost:9000/repl")
 
-(js/console.log chan)
+;; (js/console.log (+ 2 2))
+
+;; (js/console.log  (crate/html [:p.hi "HELLO"]))
+;;                                        ;(append ($ "#root") (crate/html [:p.hi "HEY"]))
+
+
+($ (fn []
+     (-> ($ :#root)
+         (inner (crate/html [:p "HEY"])))))
+
+
+
+
+
+
+
+
+
+
+
+
